39914a39e3ef3d46d654d66f8254e943242b4cfc
isman07 <isman07@yahoo.com>
2022-03-02 04:17:04 +0700

    V 0.0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Login from './pages/Login'
+import useToken from './useToken';
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+import Dashboard from './pages/sidebarcontent/Dashboard';
+import Value from './pages/sidebarcontent/Value'
+import Asset from './pages/sidebarcontent/Asset'
+import Lokasi from './pages/sidebarcontent/Lokasi'
+import Barang from './pages/sidebarcontent/Barang'
+import User from './pages/sidebarcontent/User'
+import LendReturn from './pages/sidebarcontent/LendReturn'
+
+
+
 
 function App() {
+  
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="wrapper">
+        <Header setToken={setToken}/>
+        <Menu/>
+        <Routes>
+          <Route path='/' element={<Dashboard/>}/>
+          <Route path='/value' element={<Value/>}/>
+          <Route path='/asset' element={<Asset/>}/>
+          <Route path='/lokasi' element={<Lokasi/>}/>
+          <Route path='/barang' element={<Barang/>}/>
+          <Route path='/user' element={<User/>}/>
+          <Route path='/lendreturn' element={<LendReturn/>}/>
+        </Routes>
+        <Footer/>
     </div>
+    </Router>
   );
 }
 
